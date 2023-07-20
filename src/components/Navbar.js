f0e9eda517c2e4f4aab99b8b2a80dd1220c107cf
Saad Zafar <70218806+saadzafar649@users.noreply.github.com>
2023-07-20 16:16:59 +0500

    fix: navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import logo from '../assets/6.png';
-import { AppBar, Box, Button, Divider, Drawer, IconButton, List, ListItem, ListItemButton, Toolbar, Typography } from '@mui/material';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/6.png";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 
 const drawerWidth = 240;
 const navItems = [
   ["Home", "/"],
-  ["Cart","/Cart"],
-  ["Products","/ProductList"],
+  ["Cart", "/Cart"],
+  ["Products", "/ProductList"],
 ];
 
 const Navbar = () => {
@@ -20,15 +32,17 @@ const Navbar = () => {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-       <Typography color="black" fontSize={20}>SNIX</Typography>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography color="black" fontSize={20}>
+        SNIX
+      </Typography>
       <Divider />
-      <List>
+      <List sx={{display:'flex',flexDirection:'column'}}>
         {navItems.map((item) => (
-          <NavLink className="nav-link active" aria-current="page" to={item[1]}>
-            <ListItem key={item[0]} disablePadding>
-              <ListItemButton sx={{ textAlign: 'center' }}>{item[0]}</ListItemButton>
-            </ListItem>
+          <NavLink to={item[1]}>
+            <Button key={item[0]} sx={{ fontWeight: "bold" }}>
+              {item[0]}
+            </Button>
           </NavLink>
         ))}
       </List>
@@ -37,28 +51,39 @@ const Navbar = () => {
 
   return (
     <>
-      <AppBar position="fixed" elevation={0} sx={{ background: 'transparent' }}>
-        <Toolbar sx={{ display: "flex", justifyContent: "space-between", maxHeight: '65px', minHeight: '65px' }}>
-          <Typography color="black" fontSize={20}>SNIX</Typography>
+      <AppBar position="fixed" elevation={0} sx={{ background: "transparent" }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            maxHeight: "65px",
+            minHeight: "65px",
+          }}
+        >
+          <Typography color="black" fontSize={20}>
+            SNIX
+          </Typography>
           <IconButton
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' }}}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
 
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
-                <NavLink to={item[1]}>
-              <Button key={item[0]} sx={{fontWeight:'bold'}}>
+              <NavLink to={item[1]}>
+                <Button key={item[0]} sx={{ fontWeight: "bold" }}>
                   {item[0]}
-              </Button>
-                </NavLink>
+                </Button>
+              </NavLink>
             ))}
           </Box>
-          <Box sx={{ display: { xs: 'none', sm: 'block' }, width:'60px' }}></Box>
+          <Box
+            sx={{ display: { xs: "none", sm: "block" }, width: "60px" }}
+          ></Box>
         </Toolbar>
         <Drawer
           variant="temporary"
@@ -68,8 +93,11 @@ const Navbar = () => {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
